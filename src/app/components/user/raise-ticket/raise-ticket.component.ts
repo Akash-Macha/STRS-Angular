014b5103bb35a8aec127347217d78c5c0dfb9b71
Akash Macha <akashmacha1999@gmail.com>
2019-10-12 01:09:21 +0530

    Formatted the nav bars

--- src/app/components/user/raise-ticket/raise-ticket.component.ts
+++ src/app/components/user/raise-ticket/raise-ticket.component.ts
@@ -61,12 +61,6 @@ export class RaiseTicketComponent implements OnInit {
 
         console.log("REQUESTED END DATE = " + this.newTicket.requested_end_date);
 
-        /* updating the Date w.r.t to generalized TimeZone:
-        check: https://stackoverflow.com/a/31732581 */
-        // this.newTicket.start_date =  new Date( this.newTicket.start_date.toString().replace(/-/g, '\/').replace(/T.+/, '') );
-        // this.newTicket.requested_end_date =  new Date( this.newTicket.requested_end_date.toString().replace(/-/g, '\/').replace(/T.+/, '') );
-
-        
 		this._userService.insertTicket(this.newTicket).subscribe(
 			data => {
                 this.router.navigate( ['../showAllTickets', {insertStatus: 'inserted'}], {relativeTo: this.route} );
