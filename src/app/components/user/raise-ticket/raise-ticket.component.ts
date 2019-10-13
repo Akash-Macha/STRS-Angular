1132b070b304f944378b02869168126e1f791d0c
Akash Macha <akashmacha1999@gmail.com>
2019-10-14 01:10:29 +0530

    Code cleaned up

--- src/app/components/user/raise-ticket/raise-ticket.component.ts
+++ src/app/components/user/raise-ticket/raise-ticket.component.ts
@@ -6,78 +6,103 @@ import { Ticket } from 'src/app/models/Ticket';
 import { UserService } from 'src/app/services/user-service/user.service';
 
 @Component({
-  selector: 'app-raise-ticket',
-  templateUrl: './raise-ticket.component.html',
-  styleUrls: ['./raise-ticket.component.css']
+    selector: 'app-raise-ticket',
+    templateUrl: './raise-ticket.component.html',
+    styleUrls: ['./raise-ticket.component.css']
 })
 export class RaiseTicketComponent implements OnInit {
 
     // To display
-    public listOfDepartments:Department[];
-    public listOfPriorities:Priority[];
-  
+    public listOfDepartments: Department[];
+    public listOfPriorities: Priority[];
+
     public minDate = new Date();
     // 2 years is the maximum requested date from the current Day!
-    public maxDate = new Date(this.minDate.getFullYear() + 2 , this.minDate.getMonth() , this.minDate.getDay());
-  
+    public maxDate = new Date(this.minDate.getFullYear() + 2, this.minDate.getMonth(), this.minDate.getDay());
+
     // new Ticket to be submitted
     public newTicket: Ticket;
-    
-	constructor(
-        private _userService:UserService,
-		private route:ActivatedRoute, 
-		private router:Router) { }
 
-	ngOnInit() {
-		this._userService.getListOfDepartments().subscribe(
-		  (data) => this.listOfDepartments = data,
-		  (error) => console.log(error)
-		);
-	
-		this._userService.getListOfPriorities().subscribe(
-		  data => this.listOfPriorities = data,
-		  error => console.log(error)
-		);
-	
-		// instantiating new Ticket
-		this.newTicket = new Ticket();
-	  }
+    public issueCategoryHasError: boolean = true;
+    public priorityHasError: boolean = true;
 
-	  onSubmit(){
-		for(let i =0 ; i < this.listOfDepartments.length ; ++i){
-			if(this.newTicket.category.name === this.listOfDepartments[i].name){
-				this.newTicket.category = this.listOfDepartments[i];
-				break;
-			}
-		}
-	
-		for(let i=0 ; i < this.listOfPriorities.length ; ++i){
-			if(this.newTicket.priority.value === this.listOfPriorities[i].value){
-				this.newTicket.priority = this.listOfPriorities[i];
-			}
-		}
-		// setting up the requested user_name
-		this.newTicket.requested_by.user_name = localStorage.getItem('user_name');
+    constructor(
+        private _userService: UserService,
+        private route: ActivatedRoute,
+        private router: Router) { }
 
-        console.log("REQUESTED END DATE = " + this.newTicket.requested_end_date);
+    ngOnInit() {
+        this._userService.getListOfDepartments().subscribe(
+            (data) => this.listOfDepartments = data,
+            (error) => console.log(error)
+        );
 
-		this._userService.insertTicket(this.newTicket).subscribe(
-			data => {
-                this.router.navigate( ['../showAllTickets', {insertStatus: 'inserted'}], {relativeTo: this.route} );
-			},
-			error => console.log("ERROR in _userService.insertTicket" + JSON.stringify(error))
-		);
-	  }
+        this._userService.getListOfPriorities().subscribe(
+            data => this.listOfPriorities = data,
+            error => console.log(error)
+        );
 
-	// Filtering out Saturdays and Sundays
-	dateFilter = (date: Date) =>{
-		const day = date.getDay();
-		return day != 0 && day != 6;
-	}
+        // instantiating new Ticket
+        this.newTicket = new Ticket();
+    }
 
-	// updating newTicket.requested_end_date onChange of the requested_end_date input field
-	updateRequestedEndDate(change:string, event: any){
-		this.newTicket.requested_end_date = event.value;
-	}
+    onSubmit() {
+        for (let i = 0; i < this.listOfDepartments.length; ++i) {
+            if (this.newTicket.category.name === this.listOfDepartments[i].name) {
+                this.newTicket.category = this.listOfDepartments[i];
+                break;
+            }
+        }
+
+        for (let i = 0; i < this.listOfPriorities.length; ++i) {
+            if (this.newTicket.priority.value === this.listOfPriorities[i].value) {
+                this.newTicket.priority = this.listOfPriorities[i];
+            }
+        }
+        // setting up the requested user_name
+        this.newTicket.requested_by.user_name = localStorage.getItem('user_name');
+
+        // console.log("REQUESTED END DATE = " + this.newTicket.requested_end_date);
+
+        console.log("Ticket before hitting service = " + JSON.stringify( this.newTicket ));
+        
+
+        // this._userService.insertTicket(this.newTicket).subscribe(
+        //     data => {
+        //         this.router.navigate(['../showAllTickets', { insertStatus: 'inserted' }], { relativeTo: this.route });
+        //     },
+        //     error => console.log("ERROR in _userService.insertTicket" + JSON.stringify(error))
+        // );
+    }
+
+    // AngularMaterial:  Filtering out Saturdays and Sundays
+    dateFilter = (date: Date) => {
+        const day = date.getDay();
+        return day != 0 && day != 6;
+    }
+
+    // updating newTicket.requested_end_date onChange of the requested_end_date input field
+    updateRequestedEndDate(change: string, event: any) {
+        this.newTicket.requested_end_date = event.value;
+    }
+
+    validateIssueCategory(value: any) {
+        console.log("Hit -> validateIssueCategory");
+        
+        if (value === 'default') {
+            this.issueCategoryHasError = true;
+        } else {
+            this.issueCategoryHasError = false;
+        }
+    }
+
+    validatePriority(value: any) {
+        console.log("Hit -> validatePriority");
+        if (value === 'default') {
+            this.priorityHasError = true;
+        } else {
+            this.priorityHasError = false;
+        }
+    }
 
 }
